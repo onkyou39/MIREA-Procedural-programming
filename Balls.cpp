de2c3c9d82e8f6403bddb3c57b33919cea84b5f1
onkyou39 <61075134+onkyou39@users.noreply.github.com>
2021-12-20 18:16:48 +0300

    Update Balls.cpp

--- Balls.cpp
+++ Balls.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n) {
+int factorial(int n) 
+{
     if (n > 1) return  n * factorial(n - 1);
     else return n;
 }
 
-int Mis(bool select[], int n, int step = 0) {
+int Mis(bool select[], int n, int step = 0) 
+{
     int count = 0;
-    if (step >= n) {
+    if (step >= n) 
+    {
         count = 1;
     }
     else {
-        for (int i = 0; i < n; i++) {
-            if ((i != step) and !select[i]) {
+        for (int i = 0; i < n; i++) 
+        {
+            if ((i != step) and !select[i]) 
+            {
                 select[i] = 1;
                 count += Mis(select, n, step + 1);
                 select[i] = 0;
@@ -24,7 +29,8 @@ int Mis(bool select[], int n, int step = 0) {
     return count;
 }
 
-int main() {
+int main() 
+{
     const int N = 10;
     bool select[N];
     cout << factorial(N) - Mis(select, N) << endl;
