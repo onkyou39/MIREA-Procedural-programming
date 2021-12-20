c60e3873042af71cdf932f8e6474ed288ab13245
onkyou39 <61075134+onkyou39@users.noreply.github.com>
2021-12-20 19:24:52 +0300

    Update Balls.cpp

--- Balls.cpp
+++ Balls.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n) 
+int fact(int n)
 {
-    if (n > 1) return  n * factorial(n - 1);
+    if (n > 1) return  n * fact(n - 1);
     else return n;
 }
 
-int Mis(bool select[], int n, int step = 0) 
+int perestanovka(bool select[], int n, int step = 0)
 {
     int count = 0;
-    if (step >= n) 
+    if (step >= n)
     {
         count = 1;
     }
     else {
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
         {
-            if ((i != step) and !select[i]) 
+            if ((i != step) and !select[i])
             {
                 select[i] = 1;
-                count += Mis(select, n, step + 1);
+                count += perestanovka(select, n, step + 1);
                 select[i] = 0;
             }
         }
@@ -29,10 +29,14 @@ int Mis(bool select[], int n, int step = 0)
     return count;
 }
 
-int main() 
+int main()
 {
-    const int N = 10;
-    bool select[N];
-    cout << factorial(N) - Mis(select, N) << endl;
-
+    setlocale(LC_ALL, "russian");
+    cout << "Введите количество шариков" << endl;
+    int n;
+    cin >> n;
+    bool* arr = new bool[n];
+    cout << fact(n) - perestanovka(arr, n) << endl;
+    delete[] arr;
+    return 0;
 }
