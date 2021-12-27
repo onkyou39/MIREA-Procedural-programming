3e9e61c921f4dd6e9d63074418bd6c4b5ca180fa
onkyou39 <61075134+onkyou39@users.noreply.github.com>
2021-12-27 16:57:14 +0300

    Update Spinners_5.cpp

--- Spinners_5.cpp
+++ Spinners_5.cpp
@@ -3,27 +3,28 @@
 using namespace std;
 long long pop(multiset<long long>& s)
 {
-    auto it = s.end();
-    --it;
-    int ans = *it;
-    s.erase(it);
-    return ans;
+	auto it = s.end();
+	--it;
+	int ans = *it;
+	s.erase(it);
+	return ans;
 }
 int main()
 {
-    long long n, k;
-    cin >> n >> k;
-    multiset<long long> s;
-    s.insert(n);
-    long long ans1, ans2;
-    for (long long i = 0; i < k; ++i)
-    {
-        n = pop(s);
-        ans1 = (n - 1) / 2;
-        ans2 = n - 1 - ans1;
-        s.insert(ans1);
-        s.insert(ans2);
-    }
-    cout << ans1 << endl;
-    cout << ans2 << endl;
+	long long n, k;
+	cin >> n >> k;
+	multiset<long long> s;
+	s.insert(n);
+	long long ans1, ans2;
+	for (long long i = 0; i < k; ++i)
+	{
+		n = pop(s);
+		ans1 = (n - 1) / 2;
+		ans2 = n - 1 - ans1;
+		s.insert(ans1);
+		s.insert(ans2);
+	}
+	cout << ans1 << endl;
+	cout << ans2 << endl;
+	system("pause");
 }
